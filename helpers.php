e06c36c2b3f8c255b9f6dd64d3f66cf895a6e236
Stijn Huyberechts <stijn.huyberechts@gmail.com>
2016-06-01 13:35:57 +0200

    Added check on page variable, prevents error when no result from repository.

--- helpers.php
+++ helpers.php
@@ -5,12 +5,15 @@ if (!function_exists('page_url')) {
      * Generate a url for a page
      *
      * @param \Modules\Page\Entities\Page|integer $page
-     * @return string
+     * @return string|null
      */
     function page_url($page)
     {
         if (!$page instanceof \Modules\Page\Entities\Page) {
             $page = app('Modules\Page\Repositories\PageRepository')->find($page);
+        }
+        if (!$page) {
+            return null;
         }
         return url(LaravelLocalization::getCurrentLocale() . '/' . $page->slug);
     }
